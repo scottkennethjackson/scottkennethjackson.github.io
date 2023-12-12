212afd17ed0f5c0d3d88263a7255da468b34501e
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-12-12 19:28:19 +0000

    Update dice-guesser.js

--- projects/dice-guesser/dice-guesser.js
+++ projects/dice-guesser/dice-guesser.js
@@ -3,4 +3,33 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.ceil(Math.random() * 6);
+}
 
+const getAbsoluteDistance = (target, number) => Math.abs(target - number);
+
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+    const userAbsDistance = getAbsoluteDistance(targetNumber, userGuess);
+    const computerAbsDistance = getAbsoluteDistance(targetNumber, computerGuess);
+
+    return userAbsDistance <= computerAbsDistance;
+}
+
+const updateScore = (winner) => {
+    if (winner === 'user') {
+        humanScore++;
+    } else {
+        computerScore++;
+    }
+}
+
+const checkUserInput = (input) => {
+    if (input < 1 || input > 6) {
+        alert('Your guess is out of range.');
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber++;
+}
