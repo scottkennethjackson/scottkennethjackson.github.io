413cf240323b175eca6e573d0ceae9cc29bd4fae
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-12-27 16:45:41 +0000

    Create arrays.js

---
+++ projects/loot-randomiser/arrays.js
@@ -0,0 +1,23 @@
+const common = [
+    ['item 0', 'link 0'],
+    ['item 1', 'link 1'],
+    ['item 2', 'link 2'],
+];
+
+const consumables = [
+];
+
+const uncommon = [
+];
+
+const rare = [
+];
+
+const veryRare = [
+];
+
+const cursed = [
+];
+
+const legendary = [
+];
