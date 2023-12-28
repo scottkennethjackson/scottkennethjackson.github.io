05714f7d23b91e1437138ae89bc3479b31c91f74
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-12-28 17:07:54 +0000

    Update loot-generator.js

--- projects/loot-generator/loot-generator.js
+++ projects/loot-generator/loot-generator.js
@@ -3,32 +3,32 @@ let roll2;
 
 const rollD100 = (roll1, roll2) => {
     roll1 = Math.ceil(Math.random() * 100);
+    console.log(`Roll: ${roll1}`)
 
-    if (roll1 <= 5) {
-        console.log('Nothing');
-    } else if (roll1 <= 25) {
+    if (roll1 == 1) {
+        console.log('Category: Cursed');
+    } else if (roll1 <= 11) {
+        console.log('Category: Nothing');
+    } else if (roll1 <= 26) {
+        console.log('Category: Health Potion');
+    } else if (roll1 <= 42) {
+        console.log('Category: Common');
+
         roll2 = Math.floor(Math.random() * common.length);
-        console.log('Common');
         console.log(`Item: ${common[roll2][0]}`)
         console.log(`Link: ${common[roll2][1]}`)
-    } else if (roll1 <= 40) {
-        console.log('Consumable');
-    } else if (roll1 <= 50) {
-        console.log('Uncommon');
-    } else if (roll1 <= 70) {
-        console.log('Gold');
-        console.log('1d12 x Encounter CR or Player Level')
+    } else if (roll1 <= 67) {
+        console.log('Category: Gold');
     } else if (roll1 <= 85) {
-        console.log('Healing Potion');
-        console.log('1d4 Healing Potions')
+        console.log('Category: Consumable');
     } else if (roll1 <= 93) {
-        console.log('Rare');
+        console.log('Category: Uncommon');
     } else if (roll1 <= 97) {
-        console.log('Very Rare');
+        console.log('Category: Rare');
     } else if (roll1 <= 99) {
-        console.log('Cursed');
+        console.log('Category: Very Rare');
     } else {
-        console.log('Legendary');
+        console.log('Category: Legendary');
     }
 };
 
