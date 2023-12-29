f55c733b92bdfa8ce9a827e43f478a784d0f4467
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-12-29 19:25:16 +0000

    Add consumable array

--- projects/loot-generator/arrays.js
+++ projects/loot-generator/arrays.js
@@ -1,23 +1,72 @@
+const cursed = [];
+
 const common = [
     ['item 0', 'link 0'],
     ['item 1', 'link 1'],
     ['item 2', 'link 2'],
 ];
 
-const consumables = [
+const consumable = [
+    ['Potion of Acid Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Animal Friendship', 'magic-items/4700-potion-of-animal-friendship'],
+    ['Potion of Clairvoyance', 'magic-items/4701-potion-of-clairvoyance'],
+    ['Potion of Climbing', 'magic-items/4702-potion-of-climbing'],
+    ['Potion of Cloud Giant Strength', 'magic-items/5417-potion-of-giant-strength'],
+    ['Potion of Cold Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Diminution', 'magic-items/4703-potion-of-diminution'],
+    ['Potion of Fire Breath', 'magic-items/5357-potion-of-fire-breath'],
+    ['Potion of Fire Giant Strength', 'magic-items/5417-potion-of-giant-strength'],
+    ['Potion of Fire Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Flying', 'magic-items/4704-potion-of-flying'],
+    ['Potion of Force Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Frost Giant Strength', 'magic-items/5417-potion-of-giant-strength'],
+    ['Potion of Gaseous Form', 'magic-items/4705-potion-of-gaseous-form'],
+    ['Potion of Greater Healing', 'magic-items/4708-potion-of-healing'],
+    ['Potion of Growth', 'magic-items/4707-potion-of-growth'],
+    ['Potion of Heroism', 'magic-items/4709-potion-of-heroism'],
+    ['Potion of Hill Giant Strength', 'magic-items/5417-potion-of-giant-strength'],
+    ['Potion of Invisibility', 'magic-items/4710-potion-of-invisibility'],
+    ['Potion of Invulnerability', 'magic-items/5358-potion-of-invulnerability'],
+    ['Potion of Lightning Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Longevity', 'magic-items/5359-potion-of-longevity'],
+    ['Potion of Mind Reading', 'magic-items/4711-potion-of-mind-reading'],
+    ['Potion of Necrotic Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Poison', 'magic-items/4712-potion-of-poison'],
+    ['Potion of Poison Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Psychic Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Radiant Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Speed', 'magic-items/4714-potion-of-speed'],
+    ['Potion of Stone Giant Strength', 'magic-items/5417-potion-of-giant-strength'],
+    ['Potion of Storm Giant Strength', 'magic-items/5417-potion-of-giant-strength'],
+    ['Potion of Superior Healing', 'magic-items/4708-potion-of-healing'],
+    ['Potion of Supreme Healing', 'magic-items/4708-potion-of-healing'],
+    ['Potion of Thunder Resistance', 'magic-items/5419-potion-of-resistance'],
+    ['Potion of Vitality', 'magic-items/5360-potion-of-vitality'],
+    ['Potion of Water Breathing', 'magic-items/4715-potion-of-water-breathing'],
+    ['Scroll of Protection Against Aberrations', 'magic-items/5398-scroll-of-protection'],
+    ['Scroll of Protection Against Beasts', 'magic-items/5398-scroll-of-protection'],
+    ['Scroll of Protection Against Celestials', 'magic-items/5398-scroll-of-protection'],
+    ['Scroll of Protection Against Elementals', 'magic-items/5398-scroll-of-protection'],
+    ['Scroll of Protection Against Fey', 'magic-items/5398-scroll-of-protection'],
+    ['Scroll of Protection Against Fiends', 'magic-items/5398-scroll-of-protection'],
+    ['Scroll of Protection Against Plants', 'magic-items/5398-scroll-of-protection'],
+    ['Scroll of Protection Against Undead', 'magic-items/5398-scroll-of-protection'],
+    ['Spell Scroll, Cantrip', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 1st Level', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 2nd Level', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 3rd Level', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 4th Level', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 5th Level', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 6th Level', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 7th Level', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 8th Level', 'magic-items/5418-spell-scroll'],
+    ['Spell Scroll, 9th Level', 'magic-items/5418-spell-scroll']
 ];
 
-const uncommon = [
-];
+const uncommon = [];
 
-const rare = [
-];
+const rare = [];
 
-const veryRare = [
-];
+const veryRare = [];
 
-const cursed = [
-];
-
-const legendary = [
-];
+const legendary = [];
--- projects/loot-generator/loot-generator.js
+++ projects/loot-generator/loot-generator.js
@@ -1,26 +1,42 @@
 let roll1;
 let roll2;
+let roll3;
 
-const rollD100 = (roll1, roll2) => {
-    roll1 = Math.ceil(Math.random() * 100);
-    console.log(`Roll: ${roll1}`)
+const rollButton = document.getElementById('d100');
+const itemDisplay = document.getElementById('item');
+const linkDisplay = document.getElementById('link');
+
+const rollLoot = () => {
+    roll1 = rollD100();
+    console.log(`Roll: ${roll1}`);
 
     if (roll1 == 1) {
         console.log('Category: Cursed');
     } else if (roll1 <= 11) {
-        console.log('Category: Nothing');
+        //console.log('Category: Nothing');
+        itemDisplay.innerHTML = `You Didn't Find Anything`;
+        linkDisplay.innerHTML = `<a href="#" id="link"></a>`;
     } else if (roll1 <= 26) {
-        console.log('Category: Health Potion');
+        //console.log('Category: Health Potion');
+        roll2 = rollD4();
+
+        if (roll2 == 1) {
+            itemDisplay.innerHTML = `${roll2} Potion of Healing`;
+        } else {
+            itemDisplay.innerHTML = `${roll2} Potions of Healing`;
+        }
+
+        linkDisplay.innerHTML = `<a href="https://www.dndbeyond.com/magic-items/4708-potion-of-healing" id="link" target="_blank">View Item</a>`;
     } else if (roll1 <= 42) {
         console.log('Category: Common');
-
-        roll2 = Math.floor(Math.random() * common.length);
-        console.log(`Item: ${common[roll2][0]}`)
-        console.log(`Link: ${common[roll2][1]}`)
     } else if (roll1 <= 67) {
         console.log('Category: Gold');
+        rollGold();
     } else if (roll1 <= 85) {
-        console.log('Category: Consumable');
+        //console.log('Category: Consumable');
+        roll2 = Math.floor(Math.random() * consumable.length);
+        itemDisplay.innerHTML = `${consumable[roll2][0]}`;
+        linkDisplay.innerHTML = `<a href="https://www.dndbeyond.com/${consumable[roll2][1]}" id="link" target="_blank">View Item</a>`;
     } else if (roll1 <= 93) {
         console.log('Category: Uncommon');
     } else if (roll1 <= 97) {
@@ -32,4 +48,6 @@ const rollD100 = (roll1, roll2) => {
     }
 };
 
-rollD100();
+rollButton.addEventListener('click', () => {
+    rollLoot();
+});
