580e2fde58b308b2c350dc2c8beb6e76db10bba4
scottkennethjackson <86068263+scottkennethjackson@users.noreply.github.com>
2023-12-31 19:34:07 +0000

    Update loot-generator.js

--- projects/loot-generator/js/loot-generator.js
+++ projects/loot-generator/js/loot-generator.js
@@ -12,16 +12,19 @@ const rollLoot = () => {
 
     if (roll1 == 1) {
         //console.log('Category: Cursed');
+        //console.log(`Item: ${cursed[roll2][0]}`);
         roll2 = Math.floor(Math.random() * cursed.length);
 
         itemDisplay.innerHTML = `${cursed[roll2][0]}`;
         linkDisplay.innerHTML = `<a href="https://www.dndbeyond.com/${cursed[roll2][1]}" id="link" target="_blank">View Item</a>`;
     } else if (roll1 <= 11) {
         //console.log('Category: Nothing');
+        //console.log('Item: None');
         itemDisplay.innerHTML = `You Didn't Find Anything`;
         linkDisplay.innerHTML = `<a href="#" id="link"></a>`;
     } else if (roll1 <= 26) {
         //console.log('Category: Health Potion');
+        //console.log(`Item: Potion of Healing x ${roll2}`);
         roll2 = rollD4();
 
         if (roll2 == 1) {
@@ -33,6 +36,7 @@ const rollLoot = () => {
         linkDisplay.innerHTML = `<a href="https://www.dndbeyond.com/magic-items/4708-potion-of-healing" id="link" target="_blank">View Item</a>`;
     } else if (roll1 <= 42) {
         //console.log('Category: Common');
+        //console.log(`Item: ${common[roll2][0]}`);
         roll2 = Math.floor(Math.random() * common.length);
 
         itemDisplay.innerHTML = `${common[roll2][0]}`;
@@ -42,30 +46,35 @@ const rollLoot = () => {
         rollGold();
     } else if (roll1 <= 85) {
         //console.log('Category: Consumable');
+        //console.log(`Item: ${consumable[roll2][0]}`);
         roll2 = Math.floor(Math.random() * consumable.length);
 
         itemDisplay.innerHTML = `${consumable[roll2][0]}`;
         linkDisplay.innerHTML = `<a href="https://www.dndbeyond.com/${consumable[roll2][1]}" id="link" target="_blank">View Item</a>`;
     } else if (roll1 <= 93) {
         //console.log('Category: Uncommon');
+        //console.log(`Item: ${uncommon[roll2][0]}`);
         roll2 = Math.floor(Math.random() * uncommon.length);
 
         itemDisplay.innerHTML = `${uncommon[roll2][0]}`;
         linkDisplay.innerHTML = `<a href="https://www.dndbeyond.com/${uncommon[roll2][1]}" id="link" target="_blank">View Item</a>`;
     } else if (roll1 <= 97) {
         //console.log('Category: Rare');
+        //console.log(`Item: ${rare[roll2][0]}`);
         roll2 = Math.floor(Math.random() * rare.length);
 
         itemDisplay.innerHTML = `${rare[roll2][0]}`;
         linkDisplay.innerHTML = `<a href="https://www.dndbeyond.com/${rare[roll2][1]}" id="link" target="_blank">View Item</a>`;
     } else if (roll1 <= 99) {
         //console.log('Category: Very Rare');
+        //console.log(`Item: ${veryRare[roll2][0]}`);
         roll2 = Math.floor(Math.random() * veryRare.length);
 
         itemDisplay.innerHTML = `${veryRare[roll2][0]}`;
         linkDisplay.innerHTML = `<a href="https://www.dndbeyond.com/${veryRare[roll2][1]}" id="link" target="_blank">View Item</a>`;
     } else {
         //console.log('Category: Legendary');
+        //console.log(`Item: ${legendary[roll2][0]}`);
         roll2 = Math.floor(Math.random() * legendary.length);
 
         itemDisplay.innerHTML = `${legendary[roll2][0]}`;
