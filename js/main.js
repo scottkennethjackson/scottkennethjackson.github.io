2ed42b33d3ab61722c1e762c4b15361b6d91fe9d
Scott Kenneth Jackson <scottkennethjackson@gmail.com>
2026-02-26 10:46:19 +0000

    Add dark mode styling
    This commit adds Tailwind classes for dark-preferring devices,
    including classes that are toggled on and off by JS, such as the
    form's submit button. It also replaces the project code text with
    svgs and fixes the description squashing issue by addind a max-h
    class.

--- js/main.js
+++ js/main.js
@@ -7,22 +7,24 @@ carouselLogic();
 validateForm();
 
 // Collapse/expand sections when heading button is clicked
-function toggleSection(section) {
+function toggleSection(section, button) {
     const isOpen = section.style.maxHeight;
 
     if (isOpen) {
         section.style.maxHeight = null;
         section.setAttribute("inert", "")
+        button.classList.remove("text-white", "dark:text-yellow");
     } else {
         section.style.maxHeight = section.scrollHeight + "px";
         section.removeAttribute("inert");
+        button.classList.add("text-white", "dark:text-yellow");
     }
 }
 
 document.querySelectorAll("[data-toggle]").forEach(button => {
     button.addEventListener("click", () => {
         const section = document.getElementById(button.dataset.toggle);
-        toggleSection(section);
+        toggleSection(section, button);
     });
 });
 
